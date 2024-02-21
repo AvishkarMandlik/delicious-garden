992fe3bf84ab2a17da648e0a76be3b546bd9ce90
AvishkarMandlik <mandlikavi121@gmail.com>
2024-02-21 23:40:55 +0530

    replace class to className

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -7,22 +7,22 @@ function Navbar({ user }) {
   const [foodItemCount, ] = useState(myFoodListCount)
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-dark  bg-secondary">
-        <div class="container-fluid">
-          <Link class="navbar-brand" href="#">Delicious Garden 🍔</Link>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg navbar-dark  bg-secondary">
+        <div className="container-fluid">
+          <Link className="navbar-brand" href="#">Delicious Garden 🍔</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <Link class="nav-link active" aria-current="page" href="home">Home</Link>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" href="home">Home</Link>
               </li>
             </ul>
-            <form class="d-flex align-items-center">
+            <form className="d-flex align-items-center">
               <h4 className='me-2 text-light'>Hello {user}</h4>
               <Link to="/MyList" className='text-decoration-none'>
-                <h4 className='me-2 my-list'>🍽️ {foodItemCount}</h4>
+                <h4 className='me-2 my-list'>🍽️🍕 {foodItemCount}</h4>
               </Link>
             </form>
           </div>
